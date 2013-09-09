56621c8b97dc827d93cf8aaf5340e561b2450544
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-09-09 10:23:41 -0300

    codigo separado

---
+++ livro/capitulos/code/cap1/tamando_dos_tipos.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  printf ("sizeof (unsigned int) = %d\n", sizeof (unsigned int));
+  printf ("sizeof (signed int) = %d\n", sizeof (signed int));
+  printf ("sizeof (short int) = %d\n", sizeof (short int));
+  system ("pause");
+  return 0;
+}
