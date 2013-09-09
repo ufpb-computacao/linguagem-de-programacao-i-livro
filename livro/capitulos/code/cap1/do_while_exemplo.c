6cf9d2699869ada06517cfc07ed7963a8cec0d5a
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-09-09 11:03:08 -0300

    programas

---
+++ livro/capitulos/code/cap1/do_while_exemplo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  do{
+    printf ("\nEscolha a fruta pelo numero:\n");
+    printf ("\t(1)...Mamao\n");
+    printf ("\t(2)...Abacaxi\n");
+    printf ("\t(3)...Laranja\n");
+    scanf("%d", &i);
+  } while ((i<1) || (i>3));
+
+  return 0;
+}
