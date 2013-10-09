6f67f296bd0cbbbf5247dc748e28bbfed6d9dbee
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-10-09 10:02:54 -0300

    Inclusão de imagem e código de Variável não inicializada.

---
+++ livro/capitulos/code/cap3/variavel_nao_inicializada.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	float conta; // representa o valor da conta de luz da sua casa
+	//conta=65.90;
+	printf("O valor da conta esse mês é %0.2f. \n", S);
+	system ("pause");
+	return 0;
+}
