b4b48d30443c0bd9d191d0fd4d0f7ae6b1264ce8
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-10-09 10:25:38 -0300

    Atualização do código

--- livro/capitulos/code/cap3/variavel_nao_inicializada.c
+++ livro/capitulos/code/cap3/variavel_nao_inicializada.c
@@ -4,7 +4,7 @@
 int main() {
 	float conta; // representa o valor da conta de luz da sua casa
 	//conta=65.90;
-	printf("O valor da conta esse mês é %0.2f. \n", S);
-	system ("pause");
+	printf("O valor da conta esse mês é %0.2f. \n", conta);
+	//system ("pause");
 	return 0;
 }
