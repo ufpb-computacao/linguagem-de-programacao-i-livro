8ecf57e3c6d2324579a18d1c2daac2763c337c1b
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-10-09 11:22:46 -0300

    Inclusão de operador_de_endereco.

---
+++ livro/capitulos/code/cap3/operador_de_endereco.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int pg=120;
+	double livro=145.60;
+	printf("Valor de pg = %d. \n", pg);
+	printf("Endereço de pg = %p. \n", &pg);
+	printf("Valor de livro = %lf. \n", livro);
+	printf("Endereço de livro = %p. \n", &livro);
+	//system ("pause");
+	return 0;
+}
