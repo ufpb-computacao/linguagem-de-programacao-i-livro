8c93745fbab18546b4a0804e14b7ddb149929b48
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-01-20 21:53:25 -0300

    Etapa 5 #13
    Correção ortográfica #13

--- livro/capitulos/code/cap5/etapa4/src/lingua-do-i-core.c
+++ livro/capitulos/code/cap5/etapa4/src/lingua-do-i-core.c
@@ -1,16 +1,16 @@
-#include <stdbool.h>
+#include <stdbool.h> // <1>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lingua-do-i-core.h"
 
-bool arquivoAbertoComSucesso(FILE* arquivo) {
+bool arquivoAbertoComSucesso(FILE* arquivo) {//<2>
 	return arquivo != NULL;
 }
 
 /*
 Efeito colateral: cabeçote de leitura vai para o início do arquivo
 */
-int lerTamanhoDoArquivo(FILE* arquivo) {
+int lerTamanhoDoArquivo(FILE* arquivo) {//<2>
 	fseek(arquivo, 0, SEEK_END); // vai para o final do arquivo
 	int tamanho = ftell(arquivo); // pega posição atual (final)
 
@@ -18,16 +18,18 @@ int lerTamanhoDoArquivo(FILE* arquivo) {
 	return tamanho;
 }
 
-char* lerConteudoDeArquivoArberto(FILE* arquivo) {
+char* lerConteudoDeArquivoArberto(FILE* arquivo) {//<2>
 	int tamanhoDoArquivo = lerTamanhoDoArquivo(arquivo);
-	char* conteudo = calloc(1, tamanhoDoArquivo + 1); // 7.22.3.2 The calloc function
+	// 7.22.3.2 The calloc function
+	char* conteudo = calloc(1, tamanhoDoArquivo + 1);
 
-	fread(conteudo, tamanhoDoArquivo, 1, arquivo); // 7.21.8.1 The fread function
+	// 7.21.8.1 The fread function
+	fread(conteudo, tamanhoDoArquivo, 1, arquivo);
 
 	return conteudo;
 }
 
-char* lerConteudoDoArquivo(char* nomeDoArquivo) {
+char* lerConteudoDoArquivo(char* nomeDoArquivo) {//<2>
 	char* conteudo;
 
 	FILE* arquivo = fopen(nomeDoArquivo, "r");
