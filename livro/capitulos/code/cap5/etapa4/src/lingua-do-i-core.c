c81b267e8e9adbfa7674b37b3570b024640c649a
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-01-22 09:02:21 -0300

    Inclusão de biblioteca e divisão de arquivos #13

--- livro/capitulos/code/cap5/etapa4/src/lingua-do-i-core.c
+++ livro/capitulos/code/cap5/etapa4/src/lingua-do-i-core.c
@@ -7,10 +7,9 @@ bool arquivoAbertoComSucesso(FILE* arquivo) {//<2>
 	return arquivo != NULL;
 }
 
-/*
-Efeito colateral: cabeçote de leitura vai para o início do arquivo
-*/
-int lerTamanhoDoArquivo(FILE* arquivo) {//<2>
+/*Efeito colateral:
+Cabeçote de leitura vai para o início do arquivo.*/
+int lerTamanhoDoArquivo(FILE* arquivo) {//<3>
 	fseek(arquivo, 0, SEEK_END); // vai para o final do arquivo
 	int tamanho = ftell(arquivo); // pega posição atual (final)
 
@@ -18,10 +17,10 @@ int lerTamanhoDoArquivo(FILE* arquivo) {//<2>
 	return tamanho;
 }
 
-char* lerConteudoDeArquivoArberto(FILE* arquivo) {//<2>
+char* lerConteudoDeArquivoArberto(FILE* arquivo) {//<4>
 	int tamanhoDoArquivo = lerTamanhoDoArquivo(arquivo);
 	// 7.22.3.2 The calloc function
-	char* conteudo = calloc(1, tamanhoDoArquivo + 1);
+	char* conteudo = calloc(1, tamanhoDoArquivo+1);
 
 	// 7.21.8.1 The fread function
 	fread(conteudo, tamanhoDoArquivo, 1, arquivo);
@@ -29,13 +28,13 @@ char* lerConteudoDeArquivoArberto(FILE* arquivo) {//<2>
 	return conteudo;
 }
 
-char* lerConteudoDoArquivo(char* nomeDoArquivo) {//<2>
+char* lerConteudoDoArquivo(char* nomeDoArquivo) {//<5>
 	char* conteudo;
 
-	FILE* arquivo = fopen(nomeDoArquivo, "r");
+	FILE* arquivo = fopen(nomeDoArquivo, "r"); // <6>
 	if (arquivoAbertoComSucesso(arquivo)) {
 		conteudo = lerConteudoDeArquivoArberto(arquivo);
-		fclose(arquivo); // fecha arquivo
+		fclose(arquivo); // fecha arquivo <7>
 	} else {
 		conteudo = NULL;
 	}
